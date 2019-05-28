d17c46b0a778a3f6f9d9c86eb767598cbfb9af18
Johan Gustafsson <gustajo@chalmers.se>
2019-05-28 14:42:41 +0200

    feat: Added an export script of the data needed to create the standard template.

--- DSAVEInstall.m
+++ DSAVEInstall.m
@@ -12,6 +12,7 @@ classdef DSAVEInstall
             %Add paths of all directories
             addpath(mainDir);
             addpath(strcat(mainDir,'/DataImport'));
+            addpath(strcat(mainDir,'/DataExport'));
             addpath(strcat(mainDir,'/DSAVE'));
             addpath(strcat(mainDir,'/FigureGeneration'));
             addpath(strcat(mainDir,'/GeneralFunctions'));
@@ -24,6 +25,7 @@ classdef DSAVEInstall
             %Remove paths of all directories
             rmpath(mainDir);
             rmpath(strcat(mainDir,'/DataImport'));
+            rmpath(strcat(mainDir,'/DataExport'));
             rmpath(strcat(mainDir,'/DSAVE'));
             rmpath(strcat(mainDir,'/FigureGeneration'));
             rmpath(strcat(mainDir,'/GeneralFunctions'));
