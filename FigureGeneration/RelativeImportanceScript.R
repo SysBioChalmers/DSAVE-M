83e6222443220c2d80992174954c1096e80add58
Johan Gustafsson <gustajo@chalmers.se>
2019-09-11 13:23:50 +0200

    fix: Changed the relative importance analysis to take into account how many samples a factor is represented in.

--- FigureGeneration/RelativeImportanceScript.R
+++ FigureGeneration/RelativeImportanceScript.R
@@ -16,10 +16,21 @@ summary(fit_regrData)
 # Run relative importance analysis
 results = calc.relimp(fit_regrData, type = "lmg", rank = TRUE, diff = TRUE, rela = TRUE)$lmg
 #Look at results convert to %. Copy these values to excel.
-results*100
+#results*100
 # Plot (for an overview only, the graphs are created in Excel)
-barplot(sort(results*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative explained variance (%)")
+#barplot(sort(results*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative explained variance (%)")
 
+#scale the results to balance out the unequal number of samples per factor
+#These are the values used in the paper!
+sampPerFactor = colSums(regrData)
+#remove score and intercept
+sampPerFactor = sampPerFactor[-1]
+sampPerFactor = sampPerFactor[-1]
+#divide results with samples per factor and renormalize
+scaledResults = results / sampPerFactor
+scaledResults = scaledResults / sum(scaledResults)
+scaledResults*100
+barplot(sort(scaledResults*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative explained variance (%)")
 
 ################### Remove breast cancer and run again
 rows = row.names(regrData)
@@ -34,6 +45,16 @@ summary(fit_regrData2)
 # Run relative importance analysis
 results2 = calc.relimp(fit_regrData2, type = "lmg", rank = TRUE, diff = TRUE, rela = TRUE)$lmg
 #Look at results
-results2*100
+#results2*100
 # Plot (for an overview only, the graphs are created in Excel)
-barplot(sort(results2*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative exaplained variance (%)")
+#barplot(sort(results2*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative exaplained variance (%)")
+
+#Scale the results to balance out the unequal number of samples per factor
+#These are the values used in the paper!
+sampPerFactor2 = sampPerFactor[c(1,3,4,5,6,7,8)]
+#divide results with samples per factor and renormalize
+scaledResults2 = results2 / sampPerFactor2
+scaledResults2 = scaledResults2 / sum(scaledResults2)
+scaledResults2*100
+barplot(sort(scaledResults2*100, decreasing = TRUE), las=2, ylim=c(0,100), ylab="Relative explained variance (%)")
+
